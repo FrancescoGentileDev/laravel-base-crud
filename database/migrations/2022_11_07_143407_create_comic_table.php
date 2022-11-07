fbb838b3f6dbf6792a7f17b01883c2980f022f4e
svalvastic <assogames2@gmail.com>
2022-11-07 15:53:16 +0100

    🐞 fix(migration ): change datatype

--- database/migrations/2022_11_07_143407_create_comic_table.php
+++ database/migrations/2022_11_07_143407_create_comic_table.php
@@ -13,10 +13,10 @@ class CreateComicTable extends Migration
      */
     public function up()
     {
-        Schema::create('comic', function (Blueprint $table) {
+        Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string("description");
+            $table->text("description");
             $table->string("thumb");
             $table->float("price", 6, 2);
             $table->string("series");
