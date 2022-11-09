61fb8f3101d2fc42eab39b3845e1d71c09882e41
svalvastic <assogames2@gmail.com>
2022-11-09 16:33:49 +0100

    ✨ feat(edit): button edit

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,17 +5,19 @@
 @endsection
 
 
+
 @section('content')
 <h1>{{$comic->title}}</h1>
 <img src="{{$comic->thumb}}" alt="">
 <p>
     {{$comic->description}}</p>
-    ${{$comic->price}}
+   <p> ${{$comic->price}}</p>
 
+<button onclick="window.location.href='{{ route('comics.edit', $comic->id)}}';">EDIT</button>
 <input type="button" id="deleteButton" value="DELETE">
+
 <form action="{{route('comics.destroy', ['comic'=> $comic->id])}}" method="POST" id="deleteForm">
     @csrf
     @method('DELETE')
 </form>
-
 @endsection
